2352f6d5e334b4e114bd6ca52957fe5fc1b73bc3
anunezmartinez <ahriannm00@gmail.com>
2022-07-27 07:05:26 +0200

    Initial Commit

---
+++ Places/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Places
+//
+//  Created by Adrian Nuñez Martíez on 27/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Adrian Nuñez Martíez on 27/7/22.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
